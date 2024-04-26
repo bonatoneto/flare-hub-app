2030417416a8d1dea5daacb905d2d261e3ccaefb
Neto Bonato <120340531+bonatoneto@users.noreply.github.com>
2024-04-26 17:32:50 -0300

    adding banner carousel and some responsive improvements

--- src/app/league-of-legends/times/loud/robo/page.tsx
+++ src/app/league-of-legends/times/loud/robo/page.tsx
@@ -1,24 +1,21 @@
 import Image from "next/image"
-import playerRobo from "../../../../../../public/img/teams-lol/loud/robo.png"
 
 export default function Player() {
   return (
     <main className="flex justify-center bg-neutral-900">
-    <div className="flex w-full max-w-7xl mt-32 mb-32 mr-6 ml-6 gap-6">
-      <div>
-        <Image 
-          src={playerRobo} 
+    <div className="md:flex w-full max-w-7xl sm:mt-16 mt-12 mb-32 mr-6 ml-6 gap-6">
+      <div className="flex justify-center md:min-w-56 md:max-h-main-image">
+        <img
+          src={'/img/teams-lol/loud/robo.png'} 
           alt="jogador-robo" 
-          height={1200}  
-          quality={100}
         />
       </div>
-      <div className="flex flex-col justify-between gap-6">
+      <div className="flex flex-col justify-between gap-6 mt-16 md:mt-0">
         <div className="w-full max-w-3xl flex flex-col gap-10">
-          <h1 className="text-5xl text-white font-bold">
+          <h1 className="md:text-5xl text-2xl text-white font-bold">
             Leonardo <span className="italic font-extrabold">“Robo”</span> Souza
           </h1>
-          <p className="text-white text-3xl">
+          <p className="text-white xl:text-3xl text-xl">
             Robo é o top laner da LOUD desde 2021, com passagens vitoriosas por Keyd, RED Canids, CNB, Flamengo e paiN. Hoje com 7 títulos é o maior campeão da história do CBLOL.
           </p>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,19 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "banner-mousepad": "url('/img/banner-mousepad.png')",
+        "banner-keyboard": "url('/img/banner-keyboard.png')",
+        "banner-headset": "url('/img/banner-headset.png')",
       },
+      minHeight: {
+        "main-image": "420px"
+      },
+      maxHeight: {
+        "main-image": "420px"
+      },
+      minWidth: {
+        "main-image": "64rem"
+      }
     },
   },
   plugins: [],
